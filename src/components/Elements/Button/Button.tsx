fd98b4d0f2b87cad5eba3b3cdfc0e718f86c9f61
boccochan <boccochan@gmail.com>
2022-11-09 01:28:43 +0900

    Refactor

---
+++ src/components/Elements/Button/Button.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+import { btn, primary } from "./css";
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  size: string;
+};
+
+export const Button = React.forwardRef<HTMLButtonElement, Props>(
+  function ButtonComponent(props, ref) {
+    console.log("Hello", props.size);
+
+    return (
+      <button css={[btn, primary]} ref={ref}>
+        Button
+      </button>
+    );
+  }
+);
