0c804165da7832f40c5d403210b09db4276c6773
boccochan <boccochan@gmail.com>
2022-11-09 01:45:15 +0900

    Add prettier

--- src/components/Elements/Button/Button.tsx
+++ src/components/Elements/Button/Button.tsx
@@ -1,19 +1,19 @@
-import * as React from "react";
+import * as React from 'react'
 
-import { btn, primary } from "./css";
+import { btn, primary } from './css'
 
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  size: string;
-};
+  size: string
+}
 
 export const Button = React.forwardRef<HTMLButtonElement, Props>(
   function ButtonComponent(props, ref) {
-    console.log("Hello", props.size);
+    console.log('Hello', props.size)
 
     return (
       <button css={[btn, primary]} ref={ref}>
         Button
       </button>
-    );
+    )
   }
-);
+)
